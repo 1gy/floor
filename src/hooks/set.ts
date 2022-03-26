d981ebc402b64d97622dc3b66cc3fdb2d9e687d4
1gy <42576495+1gy@users.noreply.github.com>
2022-03-26 14:53:21 +0900

    add useSet

---
+++ src/hooks/set.ts
@@ -0,0 +1,21 @@
+import { useCallback, useState } from "react";
+
+export const useSet = <T>() => {
+  const [set, setSet] = useState(() => new Set<T>());
+  const add = useCallback((item: T) => setSet((prev) => new Set([...Array.from(prev), item])), []);
+  const remove = useCallback((item: T) => setSet((prev) => new Set(Array.from(prev).filter((v) => v !== item))), []);
+  const has = useCallback((item: T) => set.has(item), [set]);
+  const toggle = useCallback(
+    (item: T) =>
+      setSet((prev) =>
+        prev.has(item) ? new Set(Array.from(prev).filter((v) => v !== item)) : new Set([...Array.from(prev), item])
+      ),
+    []
+  );
+  return {
+    add,
+    remove,
+    has,
+    toggle,
+  };
+};
