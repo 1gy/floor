3beb240c43279baf15c35877ba2522210f9fcb8e
1gy <42576495+1gy@users.noreply.github.com>
2022-04-01 23:40:30 +0900

    fix search algorithm

--- src/features/quiz/components/FlilteredMusicList.logic.ts
+++ src/features/quiz/components/FlilteredMusicList.logic.ts
@@ -1,12 +1,24 @@
 import { useMemo } from "react";
-import { useFlattenMusics } from "../../../hooks/musics";
+import { FlattenMusic, useFlattenMusics } from "../../../hooks/musics";
+
+const normalizeText = (text: string) => {
+  return text
+    .normalize("NFKC")
+    .toLowerCase()
+    .replace(/[ァ-ン]/g, (s) => String.fromCodePoint(s.codePointAt(0)! - 0x60));
+};
+
+const matchMusic = (filterText: string, music: FlattenMusic) => {
+  const text = normalizeText(filterText);
+  return normalizeText(music.artist).includes(text) || normalizeText(music.title).includes(text);
+};
 
 export const useFilteredMusicList = (filterText: string) => {
   const { musics } = useFlattenMusics();
 
   const filteredMusics = useMemo(() => {
     return musics.filter((music) => {
-      return music.artist.includes(filterText) || music.title.includes(filterText);
+      return matchMusic(filterText, music);
     });
   }, [filterText, musics]);
 
